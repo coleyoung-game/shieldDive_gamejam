240fca46dd417ae661c8981da59e5e649f9784b1
ColeYoung <ighong11@gmail.com>
2024-09-14 09:11:06 +0900

    init project.

---
+++ Assets/Script/TestLogic.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLogic : MonoBehaviour
+{
+    //private void Start()
+    //{
+    //    GameObject square = Instantiate(Resources.Load<GameObject>("Square"));
+    //    square.transform.position = Vector3.zero;
+    //}
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    transform.Rotate(Vector3.up);
+    //}
+}
